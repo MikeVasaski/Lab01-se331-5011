893eef20fdf5570f8246442201fbfc037f3ad5da
mikeJT <jeerasaktarat@gmail.com>
2022-06-30 22:54:25 +0700

    updated conditional rendering

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Boots',
-            discript: 'It is suitable to wear in the field.',
+            product: 'Shoes',
+            // discript: 'It is suitable to wear in the field.',
             image: './assets/images/socks_green.jpg',
-            cmu_link: 'https://www.cmu.ac.th/'
+            inStock: true,
+            inventory: 100,
+            onSale: true,
+            // cmu_link: 'https://www.cmu.ac.th/'
         }
     }
 })
