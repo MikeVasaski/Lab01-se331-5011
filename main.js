44d7d4568274330253ae248323e1c15affabd4dd
mikeJT <jeerasaktarat@gmail.com>
2022-06-30 23:31:43 +0700

    update clik act

--- main.js
+++ main.js
@@ -6,6 +6,22 @@ const app = Vue.createApp({
             image: './assets/images/socks_green.jpg',
             inStock: true,
             inventory: 100,
+            details: ['50% cotton', '30% wool', '20% ployester'],
+            variants: [
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
+                { id: 2235, color: 'bule', image: './assets/images/socks_bule.jpg'}
+            ],
+            cart: 0,
+            methods: {
+                addToCart() {
+                    this.cart += 1
+                },
+                updateImage(variantImage){
+                    this.image = variantImage
+                }
+            },
+
+            sizes: ['S', 'M', 'L'],
             onSale: true,
             // cmu_link: 'https://www.cmu.ac.th/'
         }
