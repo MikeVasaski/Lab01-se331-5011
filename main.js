fafd577b33012355302511a52413073a0d5a63b1
mikeJT <jeerasaktarat@gmail.com>
2022-07-01 09:26:44 +0700

    updated: all done

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Shoes',
-            // discript: 'It is suitable to wear in the field.',
+            discript: 'It is suitable to wear in the field.',
             image: './assets/images/socks_green.jpg',
             inStock: true,
-            inventory: 100,
+            inventory: 20,
             details: ['50% cotton', '30% wool', '20% ployester'],
             variants: [
                 { id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
@@ -13,16 +13,22 @@ const app = Vue.createApp({
             ],
             sizes: ['S', 'M', 'L'],
             onSale: true,
-            cmu_link: 'https://www.cmu.ac.th/',
+            cmu_link: 'https://go.camt.cmu.ac.th/',
             cart: 0,
         }
     },
     methods: {
-        addToCart() {
+        addToCart(){
             this.cart += 1
+            this.inventory -= 1
+            console.log(this.inventory)
         },
         updateImage(variantImage){
             this.image = variantImage
+        },
+        changeStute(){
+            this.inventory = this.inventory - this.cart
+            this.inStock = false
         }
     },
 })
